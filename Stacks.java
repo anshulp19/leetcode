7bc814eb8b0b98b8887f42103579ff08aa793370
Anshul Pandey <anshulpandey19@users.noreply.github.com>
2018-03-30 23:40:05 +0530

    Update Stacks.java

--- Stacks.java
+++ Stacks.java
@@ -1,7 +1,39 @@
 /**
  * Created by anshul on 17/03/18.
  */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Stack;
+
+class Interval {
+    private int start;
+    private int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
+
+class IntervalComparator implements Comparator<Interval> {
+    public int compare(Interval i1, Interval i2){
+        return i1.getStart() - i2.getStart();
+    }
+}
 
 public class Stacks {
     public void StockSpanProblem(int price[], int S[]) {
@@ -52,6 +84,79 @@ public class Stacks {
         return false;
     }
 
+    public void NextGreaterElement(int arr[]) {
+        Stack<Integer> s = new Stack<>();
+        int element, next;
+
+        s.push(arr[0]);
+
+        for(int i = 1; i < arr.length; i++) {
+            next = arr[i];
+
+            if(s.isEmpty() == false) {
+                element = s.pop();
+
+                while(element < next) {
+                    System.out.println(element + " ---> " + next);
+                    if(s.isEmpty() == true)
+                        break;
+                    element = s.pop();
+                }
+
+                if(element > next)
+                    s.push(element);
+            }
+            s.push(next);
+        }
+
+        while(s.isEmpty() ==false){
+            System.out.println(s.pop() + " ---> " + -1);
+        }
+    }
+
+    private ArrayList<Interval> mergeOverLappingIntervalsUtil(ArrayList<Interval> intervals) {
+        if(intervals.size() == 0 || intervals.size() == 1)
+            return intervals;
+
+        Collections.sort(intervals, new IntervalComparator());
+
+        Interval first = intervals.get(0);
+        int start  = first.getStart();
+        int end = first.getEnd();
+
+        ArrayList<Interval> result = new ArrayList<>();
+
+        for(int i = 1; i < intervals.size(); i++) {
+            Interval current = intervals.get(i);
+            if(current.getStart() <= end) {
+                end = Math.max(current.getEnd(), end);
+            } else {
+                result.add(new Interval(start, end));
+                start = current.getStart();
+                end = current.getEnd();
+            }
+        }
+
+        result.add(new Interval(start, end));
+
+        return result;
+    }
+
+    public void mergeOverLappingIntervals(){
+        ArrayList<Interval> x = new ArrayList<>();
+
+        x.add(new Interval(1, 3));
+        x.add(new Interval(2, 6));
+        x.add(new Interval(8, 10));
+        x.add(new Interval(15, 18));
+        x.add(new Interval(17, 20));
+
+        x = mergeOverLappingIntervalsUtil(x);
+
+        for(Interval i : x)
+            System.out.println("[" + i.getStart() + ", " + i.getEnd() + "]");
+    }
+
     public static void main(String []args) {
         int price[] = {10, 4, 5, 90, 120, 80};
         char exp[] = {'{','(',')','}','[',']'};
@@ -65,5 +170,8 @@ public class Stacks {
             System.out.println("Balanced");
         else
             System.out.println("Not Balanced");*/
+
+        //s.NextGreaterElement(price);
+        s.mergeOverLappingIntervals();
     }
 }
