602fa124806e7c511158d02f19e2702faecb6d96
Anshul Pandey <anshulpandey19@users.noreply.github.com>
2018-06-28 02:09:07 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -204,6 +204,29 @@ public class Array {
         printArray(arr);
     }
 
+    // Rearrange the array elements so that positive and negative numbers are placed alternatively
+    public void RearrangePositiveNegative(int arr[]) {
+        int i = -1, temp = 0;
+        for(int j = 0; j < arr.length; j++) {
+            if (arr[j] < 0) {
+                i++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int pos = i + 1, neg = 0;
+        while(neg < pos && pos < arr.length && arr[neg] < 0){
+            temp = arr[neg];
+            arr[neg] = arr[pos];
+            arr[pos] = temp;
+            pos++;
+            neg += 2;
+        }
+        printArray(arr);
+    }
+
     public static void main(String[] args) {
         Array a = new Array();
         int arr[] = {4, 5, 6, 7, 8, 9, 1, 2, 3};
@@ -217,5 +240,6 @@ public class Array {
         /* System.out.println(a.IsArrayConsecutive(new int[]{4, 5, 6, 7, 8, 9, 1, 2, 3}));
            System.out.println(a.IsArrayConsecutive(new int[]{5, 1, 4, 3, 6, 8, 10, 7, 9})); */
         // a.RearrangeArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
+        // a.RearrangePositiveNegative(new int[]{-1, 2, -3, 4, 5, 6, -7, 8, 9});
     }
 }
