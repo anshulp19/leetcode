cd46237a3ff30d35439c3fed4507ed8b5c12256e
Anshul Pandey <13178961+anshulp19@users.noreply.github.com>
2018-08-22 15:09:00 +0530

    Update UniquePathWithObstacles.java

--- UniquePathWithObstacles.java
+++ UniquePathWithObstacles.java
@@ -45,6 +45,23 @@ public class UniquePathWithObstacles {
         return path[row - 1][column - 1];
     }
 
+    public int uniquePathsWithObstacles_II(int[][] obstacleGrid) {
+        int width = obstacleGrid[0].length;
+        int[] dp = new int[width];
+        dp[0] = 1;
+
+        for (int[] row: obstacleGrid) {
+            for (int j = 0; j < width; j++) {
+                if (row[j] == 1)
+                    dp[j] = 0;
+                else if (j > 0)
+                    dp[j] += dp[j - 1];
+            }
+        }
+
+        return dp[width - 1];
+    }
+
     public static void main(String[] args) {
         UniquePathWithObstacles u = new UniquePathWithObstacles();
         System.out.println(u.uniquePathsWithObstacles(new int[][]{{0, 0, 0},
@@ -54,5 +71,12 @@ public class UniquePathWithObstacles {
                                                                   {0, 1, 0, 0},
                                                                   {0, 0, 1, 0},
                                                                   {0, 1, 0, 0}}));
+        System.out.println(u.uniquePathsWithObstacles_II(new int[][]{{0, 0, 0},
+                {0, 1, 0},
+                {0, 0, 0}}));
+        System.out.println(u.uniquePathsWithObstacles_II(new int[][]{{0, 0, 0, 1},
+                {0, 1, 0, 0},
+                {0, 0, 1, 0},
+                {0, 1, 0, 0}}));
     }
 }
